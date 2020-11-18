e363337e397952167815276aeb663821ea37a9f0
Miguel Castro <miguel.castro9@hotmail.com>
2020-11-18 16:22:20 -0300

    Banco de Dados definitivo

--- Banco de Dados/jpg.sql
+++ Banco de Dados/jpg.sql
@@ -24,14 +24,14 @@ DROP TABLE IF EXISTS `atividades`;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `atividades` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `medico` int(11) NOT NULL,
-  `titulo` varchar(50) NOT NULL,
-  `mensagem` text,
-  `data` date DEFAULT NULL,
-  `status` varchar(20) NOT NULL,
+  `username` varchar(40) CHARACTER SET latin1 NOT NULL,
+  `titulo` varchar(50) CHARACTER SET latin1 NOT NULL,
+  `mensagem` text CHARACTER SET latin1 NOT NULL,
+  `data` date NOT NULL,
+  `status` varchar(20) CHARACTER SET latin1 NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `medico` (`medico`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  KEY `username` (`username`)
+) ENGINE=MyISAM AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,6 +40,7 @@ CREATE TABLE `atividades` (
 
 LOCK TABLES `atividades` WRITE;
 /*!40000 ALTER TABLE `atividades` DISABLE KEYS */;
+INSERT INTO `atividades` VALUES (18,'admin2020','Realizar testes no sistma','Relatar a ausência de bugs no sistema JPG','2020-11-27','A fazer'),(11,'miguel','Projeto Integrador 3','Finalizar o Marco 3 do projeto.','2020-11-20','Em andamento'),(17,'miguel','Projeto Integrador 3','Hospedar projeto na nuvem.','2020-11-27','Em andamento'),(16,'miguel','Projeto Integrador 3','Documentar o cód. fonte completo ','2020-11-12','Em andamento');
 /*!40000 ALTER TABLE `atividades` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -58,8 +59,9 @@ CREATE TABLE `medicos` (
   `senha` varchar(50) NOT NULL,
   `crm` varchar(10) NOT NULL,
   `especializacao` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -68,6 +70,7 @@ CREATE TABLE `medicos` (
 
 LOCK TABLES `medicos` WRITE;
 /*!40000 ALTER TABLE `medicos` DISABLE KEYS */;
+INSERT INTO `medicos` VALUES (1,'Miguel','miguel castro do nascimento','miguel@jpg.com','aaaaaa','348784/sp','Cardiologista Geral'),(2,'admin2020','administrador','admin@ti.com','aaaaaa','0000000/sp','administrador do sistema'),(3,'adminadmin','admin','admin@ti.com','aaaaaa','dwadwadawd','admin');
 /*!40000 ALTER TABLE `medicos` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -80,13 +83,13 @@ DROP TABLE IF EXISTS `noticias`;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `noticias` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `medico` int(11) NOT NULL,
+  `username` varchar(40) NOT NULL,
   `titulo` varchar(50) NOT NULL,
   `mensagem` text NOT NULL,
   `data` date NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `medico` (`medico`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  KEY `medico` (`username`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -95,70 +98,46 @@ CREATE TABLE `noticias` (
 
 LOCK TABLES `noticias` WRITE;
 /*!40000 ALTER TABLE `noticias` DISABLE KEYS */;
+INSERT INTO `noticias` VALUES (1,'miguel','Reunião - JPG','Reunião com todos os Sócios e Colaboradores as 10hrs.','2020-11-19'),(2,'admin2020',' Atualização do sistema','O sistema JPG irá passar por uma atulaização em 10/11 - 00:00 ás 06:00.','2020-11-25'),(3,'miguel','xfxdfxdf','dxfxdfdxf','2020-11-25');
 /*!40000 ALTER TABLE `noticias` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `paciente`
+-- Table structure for table `triagens`
 --
 
-DROP TABLE IF EXISTS `paciente`;
+DROP TABLE IF EXISTS `triagens`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
-CREATE TABLE `paciente` (
-  `cpf` int(11) NOT NULL,
-  `nome` varchar(50) NOT NULL,
-  `data_nascimento` date NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `cep` varchar(10) NOT NULL,
-  `telefone` int(11) NOT NULL,
-  `outro_contato` varchar(40) DEFAULT NULL,
-  `sexo` enum('M','F') DEFAULT NULL,
-  PRIMARY KEY (`cpf`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `paciente`
---
-
-LOCK TABLES `paciente` WRITE;
-/*!40000 ALTER TABLE `paciente` DISABLE KEYS */;
-/*!40000 ALTER TABLE `paciente` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `triagem`
---
-
-DROP TABLE IF EXISTS `triagem`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `triagem` (
+CREATE TABLE `triagens` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_medico` int(11) NOT NULL,
-  `cpf_paciente` int(11) NOT NULL,
-  `alteracao_paladar` enum('S','N') NOT NULL,
-  `disturbios_gastrintestinais` enum('S','N') NOT NULL,
-  `falta_ar` enum('S','N') NOT NULL,
-  `tosse` enum('S','N') NOT NULL,
-  `febre` enum('S','N') NOT NULL,
-  `dor_garganta` enum('S','N') NOT NULL,
-  `dificuldade_respirar` enum('S','N') NOT NULL,
-  `perda_olfato` enum('S','N') NOT NULL,
+  `cpf` varchar(12) CHARACTER SET latin1 NOT NULL,
+  `nome` varchar(40) CHARACTER SET latin1 NOT NULL,
+  `data_nascimento` date NOT NULL,
+  `email` varchar(40) CHARACTER SET latin1 NOT NULL,
+  `cep` varchar(6) CHARACTER SET latin1 NOT NULL,
+  `estado` varchar(40) CHARACTER SET latin1 NOT NULL,
+  `contato1` varchar(40) CHARACTER SET latin1 NOT NULL,
+  `contato2` varchar(40) CHARACTER SET latin1 NOT NULL,
+  `sexo` varchar(20) CHARACTER SET latin1 NOT NULL,
+  `febre` varchar(5) CHARACTER SET latin1 NOT NULL,
+  `tosse_seca` varchar(5) CHARACTER SET latin1 NOT NULL,
+  `cansaco` varchar(5) CHARACTER SET latin1 NOT NULL,
+  `falta_ar` varchar(5) CHARACTER SET latin1 NOT NULL,
+  `pressao_peito` varchar(5) CHARACTER SET latin1 NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `cpf_paciente` (`cpf_paciente`),
-  KEY `id_medico` (`id_medico`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  UNIQUE KEY `cpf_UNIQUE` (`cpf`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `triagem`
+-- Dumping data for table `triagens`
 --
 
-LOCK TABLES `triagem` WRITE;
-/*!40000 ALTER TABLE `triagem` DISABLE KEYS */;
-/*!40000 ALTER TABLE `triagem` ENABLE KEYS */;
+LOCK TABLES `triagens` WRITE;
+/*!40000 ALTER TABLE `triagens` DISABLE KEYS */;
+INSERT INTO `triagens` VALUES (2,'168168468486','Miguel Castro','2020-11-12','teste@te.com','464866','São Paulo','4516516516468','','masculino','Sim','Não','Sim','Sim','Não');
+/*!40000 ALTER TABLE `triagens` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -170,4 +149,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-10-19 19:14:52
+-- Dump completed on 2020-11-18 16:15:38
